78ffabe059b0c0533b11c2d516bd22bf6f3bfb0b
Ronny Gonzalez <yordemblo@gmail.com>
2024-01-03 17:11:31 +0100

    Resuelto el ejercicio #0 en Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/ronnyg2121.py
@@ -0,0 +1,22 @@
+# https://es.python.org/
+
+# Esto es un comentario de una sola línea. Múy útil para explicaciones cortas y que vallan directamente al punto
+
+""" Esto es un comentario multilínea. Esto es para extenderse en las explicaciones.
+Me encantan estos en lo personal
+
+ """
+
+variable = None  # Esto es una variable
+
+CONSTANTE = 0  # Esto es una constante sintácticamente hablando. así se representan en python todo en mayúsculas y debemos respetar las reglas y no modificar su valor
+
+entero = 100  # Esto es una variable de tipo entero
+
+flotante = 1.50  # Esto es una variable de tipo flotante o decimal
+
+cadena = "Hola mundo"  # Esto es una variable de tipo texto o string
+
+booleana = True  # Esto es una variable de tippo booleana
+
+print("Hola Python")
