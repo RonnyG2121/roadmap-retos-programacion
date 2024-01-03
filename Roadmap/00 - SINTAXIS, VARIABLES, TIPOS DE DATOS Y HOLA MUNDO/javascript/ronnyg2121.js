f3563cb0c5f160e8243a27d12999daa6bbc99a02
Ronny Gonzalez <yordemblo@gmail.com>
2024-01-03 17:29:23 +0100

    resuelto el ejercicio #0 con JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/ronnyg2121.js
@@ -0,0 +1,22 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Esto es un comentario de una sola línea
+/* Esto es un comentario multilínea
+Excelente! */
+
+let variable = undefined; //esto es una variable
+const pi = 3.14; //esto es una constante. Introducida en la versión egmascript 6
+
+let entero = 10; //esto es una variable de tipo entero
+
+let decimal = 1.2; //Esto es una variable de tipo flotante o decimal
+let cadena = "Hola Moure"; //Esto es una cadena o string
+
+let booleana = true; //Esto es una variable de tipo booleana
+
+let no_definida = undefined; //Esto es una variable sin valor alguno
+
+let anulada = null; //Esta variable es de tipo nulo y hace referencia a algo que no existe
+
+
+console.log("Hola JavaScript");
